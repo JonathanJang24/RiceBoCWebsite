2f5df4655fb29ec47860b3ca1bc9b070903daf05
Jonathan Jang <jonathanjang24@gmail.com>
2023-08-05 12:32:39 -0500

    contact and about page done

--- client/src/pages/About/About.jsx
+++ client/src/pages/About/About.jsx
@@ -1,5 +1,6 @@
 import Banner from "../../components/Banner/Banner"
 import AboutImg from '../../common/images/about-page-image.jpg'
+import styles from './about.module.css'
 
 const About = () => {
     return (
@@ -9,8 +10,20 @@ const About = () => {
                 title="About"
                 subtitle="What is this webpage?"
             />
-        </div>
-        
+
+            <h1 className={styles.sectionTitle}>What is this about?</h1>
+            <div className={styles.container}>
+                <p>The idea behind this website was sparked by Christians at Rice who understood the importance of unity among the Body of Christ, along with the lack of such unity on campus. We understand the value in getting plugged into a ministry, having fellowship among brothers and sisters, as well as demonstrating God's love to those both within and outside of the Covenant Community. If non-believers cannot see the Gospel within the Church, how will they see it outside of it? To be a Christian means to be connected with other believers through a bond foreign to the world: a relationship built upon the unshakable foundation of Jesus Christ. We hope and pray that God's people around campus, and the world, can take part in worship, fellowship, and minsitry alongside one another.</p>
+            </div>
+
+            <h1 className={styles.sectionTitle}>Are you guys unitarian?</h1>
+            <div className={styles.container}>
+                <p>We are <span className={styles.bolden}>not</span> a unitarian, pantheistic, or Christian universalist movement. There are core tenants of orthodoxy that people must adhere too in order to be considered Christian. There is exclusivity amongst God's people and a clear distinction between those within and outside of the Church, as well as those who have not yet been regenerated and saved. Though as Christians we strive to "...keep our conduct excellent among the Gentiles..." (1 Peter 2:12) we will politely decline relationship to groups we deem heretical.</p>
+            </div>
+
+            <div className={styles.container}>A special shoutout to those involved in the organizing and making of this whole idea:...</div>
+
+        </div>  
     )
 }
 
--- client/src/pages/Contact/Contact.jsx
+++ client/src/pages/Contact/Contact.jsx
@@ -1,7 +1,25 @@
 import ContactImg from '../../common/images/contact-page-image.jpg'
 import Banner from '../../components/Banner/Banner'
+import styles from './contact.module.css'
+import {useState} from 'react'
 
 const Contact = () => {
+
+    const [formData, setFormData] = useState({'name':'','email':'','subject':'','description':''})
+
+    const handleChange = (event) => {
+        
+        const name = event.target.name
+        const value = event.target.value
+        setFormData(values => ({...values,[name]:value}))
+
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        console.log(formData)
+    }
+
     return (
         <div>
             <Banner
@@ -9,6 +27,16 @@ const Contact = () => {
                 subtitle="Reach out to us!"
                 photo={ContactImg}
             />
+
+            <div className={styles.formContainer}>
+                <form onSubmit={handleSubmit} className={styles.formStyle}>
+                    <input name='name' type="text" onChange={handleChange}value={formData['name']} placeholder='Name' required/>   
+                    <input name='email' type="email" onChange={handleChange} value={formData['email']} placeholder='Email' required/>
+                    <input name='subject' type="text" onChange={handleChange} value={formData['subject']} placeholder='Subject' required/>
+                    <textarea className={styles.description} name="description" onChange={handleChange} value={formData['description']} placeholder='Description' required></textarea>
+                    <button type="submit" className={styles.submitBtn}>Submit</button>
+                </form>
+            </div>
         </div>
     )
 }
