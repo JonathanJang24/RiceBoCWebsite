1628fc1fbee981bdcbc8695035ede4fd355c6557
Jonathan Jang <jonathanjang24@gmail.com>
2023-08-05 23:59:33 -0500

    google app script working for form

--- client/src/pages/Contact/Contact.jsx
+++ client/src/pages/Contact/Contact.jsx
@@ -2,28 +2,41 @@ import ContactImg from '../../common/images/contact-page-image.jpg'
 import Banner from '../../components/Banner/Banner'
 import styles from './contact.module.css'
 import {useState} from 'react'
+import classNames from 'classnames'
 
 const Contact = () => {
 
     const [formData, setFormData] = useState({'name':'','email':'','subject':'','description':''})
 
+    const [btnText,setBtnTxt] = useState("Submit")
+    const [isLoading, setIsLoading] = useState(false)
+
     const handleChange = (event) => {
-        
         const name = event.target.name
         const value = event.target.value
         setFormData(values => ({...values,[name]:value}))
-
     }
+
+    const sheetUrl = "https://script.google.com/macros/s/AKfycbzhHKbBxCAXFnQr6S7rDfoxJFSZmc9t4ymCCg9JH90flS4qXMTd9_UVRkVdqVUNhVvB/exec"
 
     const handleSubmit = (event) => {
         event.preventDefault()
-        console.log(formData)
-        fetch('https://script.google.com/macros/s/AKfycbzhHKbBxCAXFnQr6S7rDfoxJFSZmc9t4ymCCg9JH90flS4qXMTd9_UVRkVdqVUNhVvB/exec',{
-            method:'POST'
+        setBtnTxt("Sending...")
+        setIsLoading(true)
+        var form_data = new FormData();
+        for ( var key in formData ) {
+            form_data.append(key, formData[key]);
+        }
+        fetch(sheetUrl,{
+            method:'POST',
+            body: form_data
         }).then(response => {
             setFormData({'name':'','email':'','subject':'','description':''})
+            setBtnTxt("Submit")
+            setIsLoading(false) 
             return response.json()
         })
+        
     }
 
     return (
@@ -44,7 +57,7 @@ const Contact = () => {
                     <input name='email' type="email" onChange={handleChange} value={formData['email']} placeholder='Email' required/>
                     <input name='subject' type="text" onChange={handleChange} value={formData['subject']} placeholder='Subject' required/>
                     <textarea className={styles.description} name="description" onChange={handleChange} value={formData['description']} placeholder='Description' required></textarea>
-                    <button type="submit" className={styles.submitBtn}>Submit</button>
+                    <button type="submit" className={classNames(styles.submitBtn)}>{btnText}</button>
                 </form>
             </div>
         </div>
